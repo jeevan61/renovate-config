8620ef496141b77409006369aa6b1af8a38a8d22
jeevan61 <jeevan.t@viewzenlabs.com>
2026-09-12 19:44:06 +0530

    minor changes

--- runner/config.js
+++ runner/config.js
@@ -8,17 +8,6 @@ module.exports = {
 	baseBranches: ["feat/renovate-onboarding"],	// temporary
 	onboarding: false,	// temporary — skip the "add config" onboarding PR and apply our config directly
 	requireConfig: "optional",	// temporary
-	packageRules: [
-		{
-			matchUpdateTypes: ["minor", "patch"],
-			groupName: "minor + patch updates",
-		},
-		{
-			matchUpdateTypes: ["major"],
-			groupName: null,
-			automerge: false,
-		},
-	],
 	hostRules: [
 		{
 			matchHost: "npmregistry.viewzenlabs.in",
