ed84807c99860a26d9945faf3e43fe5192ac959e
jeevan61 <jeevan.t@viewzenlabs.com>
2026-09-12 20:03:04 +0530

    minor changes

--- runner/run.sh
+++ runner/run.sh
@@ -26,12 +26,14 @@ fi
 
 : "${RENOVATE_TOKEN:?Set RENOVATE_TOKEN before running (in .env or exported)}"
 : "${RENOVATE_DRY_RUN:=true}"
+: "${LOG_LEVEL:=info}"
 
 docker run --rm \
 	"${ENV_FILE_ARGS[@]}" \
 	-e RENOVATE_TOKEN="${RENOVATE_TOKEN}" \
 	-e VIEWZEN_NPM_TOKEN="${VIEWZEN_NPM_TOKEN:-}" \
 	-e RENOVATE_DRY_RUN="${RENOVATE_DRY_RUN}" \
+	-e LOG_LEVEL="${LOG_LEVEL}" \
 	-e RENOVATE_CONFIG_FILE=/usr/src/app/runner-config/config.js \
 	-v "$(pwd)/config.js:/usr/src/app/runner-config/config.js:ro" \
 	-v "$(pwd)/repositories.json:/usr/src/app/runner-config/repositories.json:ro" \
